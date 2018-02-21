baf3ee8775c0a9240ced8c72dd714c0a0d892393
Pharaoh <hamoncsl@hotmail.com>
2018-02-21 09:29:02 -0300

    Add files via upload

---
+++ test_dirty_v2.py
@@ -0,0 +1,71 @@
+#-*- coding:utf-8 -*-
+#test_map.py
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+s_width = 600
+s_height = 600
+clock = pygame.time.Clock()
+fps = 60
+running = True
+
+game_screen = pygame.display.set_mode((s_width, s_height), pygame.DOUBLEBUF)
+background = pygame.Surface(game_screen.get_size()) # Criando uma nova Surface
+background = background.convert() # Convertendo os pixels para "pygame"
+background.fill((255,0,0)) # Cor do Background
+
+class Player(pygame.sprite.DirtySprite): # Herdando Dirty_Sprites
+
+    def __init__(self):
+        
+        pygame.sprite.DirtySprite.__init__(self) # Inicializando a Herança
+        #self.image = pygame.image.load("poo1.png") # Imagem aleatoria
+        self.image = pygame.Surface((50,50)) # Criando uma nova Surface
+        self.rect = self.image.get_rect() # Criando um rect do tamanho da Surface
+        self.rect = pygame.draw.rect(self.image, (0,255,0), self.rect)
+        # Desenhando um quadrado na tela.
+
+    def update(self):
+
+        self.p_move() #Chamando p_move(), para atualizar o objeto
+        print(self.rect)
+        self.dirty = 1 # Atualiza Sprite
+
+    def p_move(self):
+        #Atualizado:
+        #Ao invez de usar update() com argumentos,
+        #p_move pega o rect.x, rect.y e o movimenta
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            self.rect.x -= 10
+        if key[pygame.K_RIGHT]:
+            self.rect.x += 10
+        if key[pygame.K_UP]:
+            self.rect.y -= 10
+        if key[pygame.K_DOWN]:
+            self.rect.y += 10
+
+player = Player()
+
+allsprites = pygame.sprite.LayeredDirty(player) # Adicionando o Objeto ao "grupo"
+allsprites.clear(game_screen, background) # Limpando a tela
+
+while(running):
+
+    clock.tick(fps)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    allsprites.update()
+
+    rects = allsprites.draw(game_screen) # Desenhando os objetos do grupo na tela
+    pygame.display.update(rects) # Atualizando os objetos do grupo
+    #print(clock.get_fps())
+
+pygame.quit()
